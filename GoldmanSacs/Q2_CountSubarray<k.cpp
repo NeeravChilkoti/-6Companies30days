cdfc6991da4c69c8216c206326c6556935f7a87a
NeeravChilkoti <43319085+NeeravChilkoti@users.noreply.github.com>
2022-01-04 21:22:32 +0530

    Create Q2_CountSubarray<k.cpp

---
+++ GoldmanSacs/Q2_CountSubarray<k.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long long multiply=1;
+        int count=0,start=0,end=0;
+        for(start=0,end=0;end<n;end++)
+        {
+            multiply=multiply*a[end];
+            while(start<end && multiply>=k)
+            {
+                multiply=multiply/a[start++];
+            }
+            if(multiply<k)
+            {
+                count+=1+end-start;
+            }
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
